83d5e762d2f15dc68b00f771a590c1910dee4023
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-05-29 21:44:57 -0400

    pixels

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ function getPixel(canvas,x,y){
     var gc=(o)=>data.data[y*(image.width*4)+x*4+o]
     return {r:gc(0),g:gc(1),b:gc(2),a:gc(3)}
 }
-function drawPixel(canvas,x,y){
+function drawPixel(canvas,x,y,color){
     var w=canvas.width
     var data=canvas.getImageData(0,0,w,canvas.height)
+    var sc=(o,v)=>data.data[y*(image.width*4)+x*4+o]=v
+    Object.keys(color).forEach((c,i)=>sc(i,color[c]))
+    canvas.putImageData(data,0,0)
 }
