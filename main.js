ba39adc526c5d08043a24ddf881c161e132e665a
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-05-30 19:58:45 -0400

    eod

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-import {Image,createCanvas} from 'canvas'
-function getPixel(canvas,x,y){
-    var w=canvas.width
-    var data=canvas.getImageData(0,0,w,canvas.height)
-    var gc=(o)=>data.data[y*(image.width*4)+x*4+o]
-    return {r:gc(0),g:gc(1),b:gc(2),a:gc(3)}
+import sdl from '@kmamal/sdl'
+
+const window = sdl.video.createWindow({ resizable: true })
+var newFrame;
+const redraw = () => {
+	const { pixelWidth: width, pixelHeight: height } = window
+	const stride = width * 4
+	const buffer = Buffer.alloc(stride * height)
+	//buf=[x1:[y1,y2,y3,y4,y5,y6]]
+	for(var i=0;i<buffer.length;i=i+4){
+		var x=Math.trunc(Math.trunc(i/4)/width)
+		var y=Math.trunc(i/4)%width
+		if(Math.floor(256 * y / height)+Math.floor(256 * x / width)==0)console.log(x,y)
+		buffer[i]= Math.floor(256 * y / height)
+		buffer[i+1]= Math.floor(256 * x / width)
+		buffer[i+2]=0
+		buffer[i+3]=255
+	}
+	console.log('newFrame')
+    newFrame=[width, height, stride, 'rgba32', buffer]
+	
 }
-function drawPixel(canvas,x,y,color){
-    var w=canvas.width
-    var data=canvas.getImageData(0,0,w,canvas.height)
-    var sc=(o,v)=>data.data[y*(image.width*4)+x*4+o]=v
-    Object.keys(color).forEach((c,i)=>sc(i,color[c]))
-    canvas.putImageData(data,0,0)
-}
+
+window
+    .on('*',()=>{window.render(...newFrame)})
+	.on('resize', redraw)
+	.on('expose', redraw)
