eb44c030fc04c7254cb4ff0d0f0bb5711703f15a
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-05-29 20:01:19 -0400

    eod

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import {Image,createCanvas} from 'canvas'
+function getPixel(canvas,x,y){
+    var w=canvas.width
+    var data=canvas.getImageData(0,0,w,canvas.height)
+    var gc=(o)=>data.data[y*(image.width*4)+x*4+o]
+    return {r:gc(0),g:gc(1),b:gc(2),a:gc(3)}
+}
+function drawPixel(canvas,x,y){
+    var w=canvas.width
+    var data=canvas.getImageData(0,0,w,canvas.height)
+}
