adb9ce03db3087c72cbbf8eec9dcee43d4c15e87
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-06-07 17:57:53 -0400

    events,depth and the like

--- gfx.js
+++ gfx.js
@@ -1,28 +1,28 @@
-const scale = (array, factor) => {
-  const scaled = [];
-
-  for (const row of array) {
-    let x = [];
-
-    for (const item of row) x.push.apply(x, Array(factor).fill(item));
-
-    scaled.push.apply(scaled, Array(factor).fill(x));
-  }
-
-  return scaled;
+import Canvas from "canvas";
+import sdl from "@kmamal/sdl";
+import util from "util";
+const processArr = (array, factor, shape) => {
+  return (
+    array
+      /*.map((r) => {
+      return Array(Math.ceil(factor[0])).fill(
+        r.flatMap((i) => Array(Math.ceil(factor[1])).fill(i))
+      );
+    })*/
+      .flat(1)
+  );
 };
 export class GFX {
-  constructor(ctx, screenSize) {
-    this.ctx = ctx;
-    this.screen = screenSize;
-    this.dimensions = [ctx.canvas.width, ctx.canvas.height];
-    this.scale = this.dimensions.map((val, i) => val / screenSize[i]);
+  constructor(screen) {
+    this.ctx = screen.ctx;
+    this.screen = screen;
   }
-  draw(x, y, pixels) {
-    this.ctx.putImageData(
+  draw(x, y, z, pixels) {
+    this.screen.draw(
       Canvas.createImageData(pixels.getData(), ...pixels.getShape()),
       x,
-      y
+      y,
+      z
     );
   }
   fillScreen(color) {
@@ -34,22 +34,102 @@ export class GFX {
     this.ctx = ctx;
   }
 }
-class PixelArray {
+export class PixelArray {
   constructor(w, h) {
     this.obj = { data: [], shape: [w, h, 4] };
   }
   set(x, y, value) {
     this.obj.data[x * this.obj.shape[0] + y] = value;
   }
-  getData(scale) {
-    scale = scale || 1;
-    var r = new Uint8ClampedArray(
-      this.obj.shape[0] * this.obj.shape[1] * 4 * scale
-    );
-    r.set(this.obj.data.flat(1), 0);
+  getData() {
+    var arr = processArr(this.obj.data);
+    var r = new Uint8ClampedArray(arr.length);
+    r.set(arr, 0);
     return r;
   }
   getShape() {
     return this.obj.shape;
+  }
+}
+export class Screen {
+  constructor(dimensions, scale, title) {
+    const window = (this.window = sdl.video.createWindow({
+      resizable: false,
+      accelerated: true,
+      title,
+      width: dimensions[0],
+      height: dimensions[1],
+    }));
+    this.events = { keyboard: {}, mouse: {} };
+    this.canvas = Canvas.createCanvas(...dimensions);
+    this.ctx = this.canvas.getContext("2d");
+    this.scale = scale;
+    this.objects = new Map();
+    this.dimensions = dimensions;
+    this.mouse = [];
+    var gameLoop = setInterval(() => {
+      if (window.destroyed) {
+        clearInterval(gameLoop);
+        return;
+      }
+      this.handleEvents();
+      this.onFrame();
+      this.drawObjects();
+      window.render(
+        ...this.dimensions,
+        this.dimensions[0] * 4,
+        "bgra32",
+        this.canvas.toBuffer("raw")
+      );
+    }, 16.7);
+  }
+  draw(pixels, x, y, z) {
+    this.objects.set(z, this.objects.get(z) || []);
+    this.objects.get(z).push({ x, y, pixels });
+  }
+  drawObjects() {
+    var zaxis = Array.from(this.objects.keys()).sort((a, b) => a - b);
+    zaxis.forEach((z) =>
+      this.objects
+        .get(z)
+        .forEach((item) => this.ctx.putImageData(item.pixels, item.x, item.y))
+    );
+  }
+  onFrame() {}
+  registerEvent(event, handler) {
+    this.events[event.split(".")[0]] = this.events[event.split(".")[0]] || {};
+    this.events[event.split(".")[0]][event.split(".")[1]] =
+      this.events[event.split(".")[0]][event.split(".")[1]] || [];
+    this.events[event.split(".")[0]][event.split(".")[1]].push(handler);
+  }
+  handleEvents() {
+    var events = this.events;
+    this.mouse.push(sdl.mouse.position);
+    var keys = sdl.keyboard.getState();
+    Object.keys(events.keyboard).forEach((key) => {
+      if (keys[sdl.keyboard.SCANCODE[key]])
+        events.keyboard[key].forEach((f) => f(this));
+    });
+    Object.keys(events.mouse).forEach((ev) => {
+      if (
+        ["button.left", "button.right", "button.middle"].includes(ev) &&
+        sdl.mouse.getButton(sdl.mouse.BUTTON[ev.split(".")[1].toUpperCase()])
+      ) {
+        events.mouse[ev].forEach((f) => f(this));
+      }
+      if (ev == "move") {
+        //the following if statement was sponsered by bad langauge design choices
+        if (
+          this.mouse.length > 1 &&
+          !util.isDeepStrictEqual(
+            this.mouse[this.mouse.length - 1],
+            this.mouse[this.mouse.length - 2]
+          )
+        )
+          events.mouse[ev].forEach((f) =>
+            f(this.mouse[this.mouse.length - 2], this)
+          );
+      }
+    });
   }
 }
--- main.js
+++ main.js
@@ -1,27 +1,13 @@
-import sdl from "@kmamal/sdl";
-import Canvas from "canvas";
-import ndarray from "ndarray";
-const window = sdl.video.createWindow({ resizable: true });
+import { GFX, PixelArray, Screen } from "./gfx.js";
 
-let canvas;
-let ctx;
-var arr = new PixelArray(100, 100);
-for (var i = 0; i < 100; i++) {
-  for (var j = 0; j < 100; j++) arr.set(i, j, [255, 255, 255, 255]);
-}
-const redraw = () => {
-  const { pixelWidth: width, pixelHeight: height } = window;
-
-  //console.log(arr.getData())
-  drawArray(arr, 50, 50);
-  const buffer = canvas.toBuffer("raw");
-  window.render(width, height, width * 4, "bgra32", buffer);
-};
-
-window.on("expose", redraw);
-
-window.on("resize", ({ pixelWidth: width, pixelHeight: height }) => {
-  canvas = Canvas.createCanvas(width, height);
-  ctx = canvas.getContext("2d");
-  redraw();
+var screen = new Screen([320, 180], [1, 1], "Game");
+var square = new PixelArray(100, 100);
+const gfx = new GFX(screen);
+screen.registerEvent("mouse.move", (mouse) => {
+  console.log("mouse move!", mouse);
 });
+for (var i = 0; i < 100; i++) {
+  for (var j = 0; j < 100; j++) square.set(i, j, [255, 255, 255, 255]);
+}
+gfx.draw(0, 0, 0, square);
+gfx.draw(10, 5, -1, square);
